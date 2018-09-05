e4e9e58ff078174b97c301ce0f3035d26b7629fc
Madeleine Huish <madeleinehuish@gmail.com>
2018-09-04 18:01:03 -0700

    completed horiz and vertical sections of crossword matrix

--- _Arrays/crosswordMatrix/index.js
+++ _Arrays/crosswordMatrix/index.js
@@ -1,12 +1,13 @@
 function crosswordMatrix(arr, word, rowLength, colLength) {
-	let pathQueue = []; //array of arrays to check
-
+	let wordCheck = []; //array of arrays to check
+	let wordcount = 0;
   // let horizontal = [''];
 	let horizontal = new Array(colLength).fill('')
 	// let vertical = [''];
 	let vertical = new Array(rowLength).fill('')
 	let downright = [];
 	let upright = [];
+	let found = false;
 
 	let counterRow = 0;
 	let counterCol = 0;
@@ -25,6 +26,24 @@ function crosswordMatrix(arr, word, rowLength, colLength) {
 	}
 	console.log('horizontal: ', horizontal);
 	console.log('vertical: ', vertical);
+	//build wordCheck
+	wordCheck = [...horizontal, ...vertical]; //also add diagonals here...
+	//check each
+	for(let elem of wordCheck) {
+		if(elem.includes(word)) {
+			wordcount++;
+			found = true;
+		}
+		if(elem.split('').reverse().join('').includes(word)) {
+			wordcount++;
+			found = true;
+		}
+		console.log('word: ', word);
+		console.log('elem: ', elem);
+	}
+	console.log('wordCheck: ', wordCheck);
+	console.log('wordcount: ', wordcount);
+	return found;
 }
 
 module.exports = crosswordMatrix ;
