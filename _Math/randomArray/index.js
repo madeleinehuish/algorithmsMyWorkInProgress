fd395ea646e666b08f24e1bd7bff8b3f89919471
Madeleine Huish <madeleinehuish@gmail.com>
2018-10-13 14:52:59 -0700

    started work on randomized arrays

---
+++ _Math/randomArray/index.js
@@ -0,0 +1,40 @@
+//this shuffle function is from stack overflow:
+//https://stackoverflow.com/questions/5836833/create-a-array-with-random-values-in-javascript
+
+//this is a work in progress...
+
+function shuffle(num) {
+	let a = [];
+	for (let i=0;i<num;++i) a[i]=i;
+
+	// http://stackoverflow.com/questions/962802#962890
+	function shuf(array) {
+	  var tmp, current, top = array.length;
+	  if(top) while(--top) {
+	    current = Math.floor(Math.random() * (top + 1));
+	    tmp = array[current];
+	    array[current] = array[top];
+	    array[top] = tmp;
+	  }
+	  return array;
+	}
+
+	return shuf(a);
+}
+
+function insertZero(arr) {
+	let randomNum = Math.ceil(Math.random(arr.length) * arr.length);
+	// console.log('randomNum: ', randomNum);
+	let index = randomNum;
+	let savedNumber = arr[randomNum];
+
+	arr[index] = 0;
+
+	return { index, savedNumber, arr };
+}
+
+function findZeroInArray(arr) {
+
+}
+
+module.exports = { shuffle, insertZero, findZeroInArray };
---
+++ _Math/randomArray/test.js
@@ -0,0 +1,13 @@
+const { shuffle, insertZero, findZeroInArray } = require('./index')
+
+describe("function shuffle successfully builds random array", () => {
+ it("Should return true", () => {
+  // expect(shuffle(10)).toEqual([2, 3, 5, 7]) ;
+ });
+});
+
+describe("function insertZero successfully inserts zero into array randomly", () => {
+ it("Should return return true", () => {
+  // expect(primesOptimal(10)).toEqual([2, 3, 5, 7]) ;
+ });
+});
