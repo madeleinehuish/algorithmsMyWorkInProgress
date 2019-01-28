d34aef04997bae520b9fa854423846e5793ed385
Madeleine Huish <madeleinehuish@gmail.com>
2019-01-28 05:37:44 -0800

    updates

--- _Arrays/longestIncreasingSeqInArray/index.js
+++ _Arrays/longestIncreasingSeqInArray/index.js
@@ -2,22 +2,17 @@
 function longestIncreasingSequence(arr) {
    if(!Array.isArray(arr)) return -1;
 
-   let max = 0;
-   let maxArr = [];
-   let current = 0;
    let currArr = [arr[0]];
+   let maxArr = [];
 
    for(let i = 1; i < arr.length; i++) {
      if(arr[i] > arr[i-1]) {
-       current++;
        currArr.push(arr[i]);
 
-       if(current > max) {
-         max=current;
+       if(currArr.length > maxArr.length) {
          maxArr=currArr
        }
      } else {
-       current = 0;
        currArr=[arr[i]];
      }
    }
